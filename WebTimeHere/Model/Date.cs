9da5bb2df591881310a7b1b2d2deeb9361139996
jonatas1096 <jonatas1096@hotmail.com>
2024-12-12 13:48:27 -0300

    Starting point

---
+++ WebTimeHere/Model/Date.cs
@@ -0,0 +1,17 @@
+﻿namespace WebTimeHere.Model
+{
+    public class Date
+    {
+        public int id { get; set; }
+        public string date { get; set; }
+        public double entrada { get; set; }
+        public double intervalo { get; set; }
+        public double fimIntervalo { get; set; }
+        public double saida { get; set; }
+        public double horasExtras { get; set; }
+        public double horasDevendo { get; set; }
+        public string? anotacao { get; set; }
+        public int fkFuncionario { get; set; }
+
+    }
+}
---
+++ WebTimeHere/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebTimeHere.Model
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
